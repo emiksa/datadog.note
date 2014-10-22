87ba4ca9ec111e225b4053a513c5b399991a0d60
mar3 <irisawa.masaru@gmail.com>
2014-10-22 21:35:03 +0900

    wip

---
+++ code.example/checks/usertraffic/usertraffic.py
@@ -0,0 +1,149 @@
+# coding: utf-8
+
+
+
+
+import sys
+import subprocess
+import json
+import codecs
+import random
+from checks import AgentCheck
+
+
+class UserTrafficCheck(AgentCheck):
+
+	@staticmethod
+	def _read_port_number(current_chain, field):
+
+		if current_chain == 'INPUT':
+			if field.startswith('dpt:'):
+				return field[4:]
+		elif current_chain == 'OUTPUT':
+			if field.startswith('spt:'):
+				return field[4:]
+
+		return 0
+
+	@staticmethod
+	def _netfilter():
+
+		command = ['sudo', '-u', 'root', '/sbin/iptables',
+				'--list', '-nvx', '--line-numbers']
+
+		stream = subprocess.Popen(
+				command,
+				stdout=subprocess.PIPE).stdout
+
+		current_chain = ''
+
+		result = {}
+		result['INPUT'] = {}
+		result['OUTPUT'] = {}
+		result['FORWARD'] = {}
+
+		for line in stream:
+
+			line = line.strip()
+			if line == '':
+				continue
+
+			fields = line.split()
+
+			if fields[0] == 'Chain':
+
+				#
+				# changing current chain
+				#
+				current_chain = fields[1]
+
+				result[current_chain] = {}
+
+			elif 11 <= len(fields) and fields[3] == 'ACCEPT':
+
+				#
+				# reading rule and status
+				#
+				num = fields[0]
+				port = UserTrafficCheck._read_port_number(current_chain, fields[11])
+				if port == 0:
+					continue
+				new_entry = {
+					'bytes': fields[2],
+					'protocol': fields[4],
+					'port': port,
+				}
+
+				result[current_chain][num] = new_entry
+
+		stream.close()
+		return result
+
+	@staticmethod
+	def _analyze_item(direction, item):
+
+		#
+		# chain
+		#
+		direction = {'INPUT': 'in', 'OUTPUT': 'out'}.get(direction)
+		if direction is None:
+			return '', 0
+
+		#
+		# protocol
+		#
+		protocol = item.get('protocol')
+		if not {'tcp': 1, 'udp': 1}.has_key(protocol):
+			return '', 0
+
+		#
+		# bytes
+		#
+		if not item.has_key('bytes'):
+			return '', 0
+		bytes = int(item['bytes'])
+
+		#
+		# port
+		#
+		if not item.has_key('port'):
+			return '', 0
+
+		key = 'user.net.traffic'
+
+		tags = [
+			'protocol:' + item['protocol'],
+			'direction:' + direction,
+			'port:' + str(item['port'])
+		]
+
+		return key, bytes, tags
+
+	def check(self, instance):
+
+		result = UserTrafficCheck._netfilter()
+
+		for chain, entries in result.iteritems():
+
+			for number, item in entries.iteritems():
+
+				key, bytes, tags = UserTrafficCheck._analyze_item(chain, item)
+				if key is None or key == '':
+					continue
+
+				#
+				# sending
+				#
+				self.gauge(key, bytes, tags = tags)
+
+		return
+
+		self.gauge(
+			'test',
+			random.random() * 1000,
+			tags = {
+				'protocol:tcp',
+				'direction:in',
+				'port:80'
+			}
+		)
